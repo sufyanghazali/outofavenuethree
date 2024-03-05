62e4c0a45c4783e6ccfcb18b54f6fbcae8d65428
Sufyan <sufyan.ghazali@gmail.com>
2024-03-05 11:39:34 +0800

    Implementing mega menu

--- script.js
+++ script.js
@@ -12,8 +12,19 @@ function closeMobileNavbar() {
 }
 
 navbarToggle.addEventListener("click", () => {
-  console.log("clickeds")
+  console.log("clickeds");
+
   navbar.classList.contains("is-open")
     ? closeMobileNavbar()
     : openMobileNavbar();
+
+  testingMegaMenu.classList.remove("active");
+});
+
+const testingButton = document.querySelector(".testing");
+const testingMegaMenu = document.querySelector(".mega-menu--testing");
+
+testingButton.addEventListener("click", () => {
+  console.log("mega menu");
+  testingMegaMenu.classList.toggle("active");
 });
